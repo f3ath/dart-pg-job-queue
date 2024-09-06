f1c95c5bb533aba1ee84a25984de47ec0b5631b3
f3ath <karapetov@gmail.com>
2024-09-05 20:31:43 -0700

    WIP

--- lib/pg_job_queue.dart
+++ lib/pg_job_queue.dart
@@ -1,22 +1,73 @@
 library pg_job_queue;
 
 import 'package:postgres/postgres.dart';
+import 'package:uuid/uuid.dart';
 
 class PgJobQueue {
-  PgJobQueue(this._connection, {String schema = 'default'})
-      : _insert = Sql.named('insert'
-            ' into $schema.jobs (type, payload, status)'
-            ' values (@type, @payload, @status)'
-            ' returning id'),
-        _fetch = Sql.named(
-            'select type, payload, status from $schema.jobs where id = @id');
-  final Connection _connection;
-  final Sql _insert;
-  final Sql _fetch;
+  PgJobQueue(
+    this._connection, {
+    String schema = 'default',
+    String table = 'jobs',
+    String defaultQueue = 'default',
+    Duration defaultTimeout = const Duration(minutes: 30),
+    String Function()? uniqueId,
+  })  : _uniqueId = uniqueId ?? Uuid().v4,
+        _defaultQueue = defaultQueue,
+        _defaultTimeout = defaultTimeout,
+        _insert = Sql.named('INSERT'
+            ' INTO $schema.$table (id, queue, payload, status)'
+            ' VALUES (@id, @queue, @payload, @status)'
+            ' RETURNING id'),
+        _fetch = Sql.named('SELECT * FROM $schema.$table WHERE id = @id'),
+        _acquire = Sql.named('UPDATE $schema.$table SET'
+            ' status = @new_status,'
+            ' worker_id = @worker,'
+            ' updated_at = now(),'
+            ' processing_started_at = now(),'
+            ' timeout = @timeout'
+            ' WHERE id = (SELECT id FROM $schema.jobs WHERE queue = @queue and status = @status ORDER BY created_at LIMIT 1)'
+            ' RETURNING *'),
+        _complete = Sql.named('UPDATE $schema.$table SET'
+            ' status = @status,'
+            ' result = @result,'
+            ' updated_at = now()'
+            ' WHERE id = @id'),
+        _init = [
+          'CREATE TABLE IF NOT EXISTS $schema.$table ('
+              ' id TEXT PRIMARY KEY,'
+              ' queue TEXT NOT NULL,'
+              ' payload JSONB NOT NULL,'
+              ' status TEXT NOT NULL,'
+              ' worker_id TEXT,'
+              ' result JSONB,'
+              ' created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,'
+              ' updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,'
+              ' processing_started_at TIMESTAMPTZ,'
+              ' timeout INTERVAL'
+              ' );'
+        ];
 
-  Future<String> add(String type, Map<String, Object?> payload) async {
+  final Connection _connection;
+  final Duration _defaultTimeout;
+  final Sql _insert, _fetch, _acquire, _complete;
+  final List<String> _init;
+  final String Function() _uniqueId;
+  final String _defaultQueue;
+
+  /// Returns the list of SQL statements to initialize the job queue.
+  List<String> get initStatements => _init.toList(growable: false);
+
+  /// Initializes the job queue.
+  Future<void> init() async {
+    for (final sql in initStatements) {
+      await _connection.execute(sql);
+    }
+  }
+
+  Future<String> add(Map<String, Object?> payload, {String? queue}) async {
     final r = await _connection.execute(_insert, parameters: {
-      'type': type,
+      'id': _uniqueId(),
+      'queue': queue ?? _defaultQueue,
       'payload': payload,
       'status': Status.scheduled.toString(),
     });
@@ -25,27 +76,63 @@ class PgJobQueue {
 
   Future<Job?> fetch(String id) async {
     final r = await _connection.execute(_fetch, parameters: {'id': id});
-    if (r.isEmpty) return null;
-    final row = r.first.toColumnMap();
-    return Job(
-      id,
-      row['type'],
-      row['payload'],
-      Status.values.firstWhere((e) => e.toString() == row['status']),
-    );
+    return r.isEmpty ? null : r.first.toJob();
+  }
+
+  Future<Job?> acquire(
+      {String? queue, Duration? timeout, String? worker}) async {
+    final r = await _connection.execute(_acquire, parameters: {
+      'queue': queue ?? _defaultQueue,
+      'worker': worker,
+      'status': Status.scheduled.toString(),
+      'new_status': Status.processing.toString(),
+      'timeout': TypedValue(Type.interval, timeout ?? _defaultTimeout),
+    });
+    return r.isEmpty ? null : r.first.toJob();
+  }
+
+  Future<void> complete(String id,
+      {Map<String, Object?> result = const {}}) async {
+    await _connection.execute(_complete, parameters: {
+      'id': id,
+      'status': Status.completed.toString(),
+      'result': result,
+    });
   }
 }
 
+extension on Interval? {
+  Duration? get duration =>
+      this == null ? null : Duration(microseconds: this!.microseconds);
+}
+
+extension on ResultRow {
+  Job toJob() => switch (toColumnMap()) {
+        {
+          'id': String id,
+          'queue': String queue,
+          'payload': Map<String, Object?> payload,
+          'status': String status,
+          'result': Map<String, Object?>? result,
+          'timeout': Interval? timeout,
+        } =>
+          Job(id, queue, payload,
+              Status.values.firstWhere((e) => e.toString() == status),
+              result: result, timeout: timeout.duration),
+        _ => throw StateError('Invalid row: $this'),
+      };
+}
+
 class Job {
-  Job(this.id, this.type, this.payload, this.status,
+  Job(this.id, this.queue, this.payload, this.status,
       {this.result, this.timeout});
 
   final String id;
-  final String type;
+  final String queue;
   final Map<String, Object?> payload;
   final Map<String, Object?>? result;
   final Status status;
   final Duration? timeout;
 }
 
-enum Status { scheduled, processing, done, failed }
+enum Status { scheduled, processing, completed, failed }
--- test/functional_test.dart
+++ test/functional_test.dart
@@ -7,45 +7,48 @@ import 'create_connection.dart';
 /// To run locally, start postgres:
 /// `docker run -p 5432:5432 -e POSTGRES_PASSWORD=postgres postgres`
 void main() async {
+  const schema = 'test';
   setUp(() async {
     final connection = await createConnection();
     await connection.execute('DROP SCHEMA IF EXISTS $schema CASCADE;');
     await connection.execute('CREATE SCHEMA IF NOT EXISTS $schema;');
-    for (final sql in init) {
-      await connection.execute(sql);
-    }
+    await PgJobQueue(connection, schema: schema).init();
   });
 
   test('Happy path', () async {
     final connection = await createConnection();
     final queue = PgJobQueue(connection, schema: schema);
-    final payload = {'foo': 123, 'bar': 'baz'};
-    final id = await queue.add('my_type', payload);
-    final job = await queue.fetch(id);
-    expect(job?.payload, equals(payload));
-    expect(job?.type, equals('my_type'));
-    expect(job?.status, equals(Status.scheduled));
-    expect(job?.result, isNull);
-    expect(job?.timeout, isNull);
-    expect(job?.id, isNotEmpty);
+    final payload = {'foo': 123, 'bar': 'baz.md'};
+    final id = await queue.add(payload);
+
+    await queue.fetch(id).then((it) {
+      expect(it?.payload, equals(payload));
+      expect(it?.queue, equals('default'));
+      expect(it?.status, equals(Status.scheduled));
+      expect(it?.result, isNull);
+      expect(it?.timeout, isNull);
+      expect(it?.id, equals(id));
+    });
+
+    await queue
+        .acquire(worker: 'worker1', timeout: Duration(seconds: 30))
+        .then((it) {
+      expect(it?.payload, equals(payload));
+      expect(it?.queue, equals('default'));
+      expect(it?.status, equals(Status.processing));
+      expect(it?.result, isNull);
+      expect(it?.timeout, equals(Duration(seconds: 30)));
+      expect(it?.id, equals(id));
+    });
+
+    await queue.complete(id);
+    await queue.fetch(id).then((it) {
+      expect(it?.payload, equals(payload));
+      expect(it?.queue, equals('default'));
+      expect(it?.status, equals(Status.completed));
+      expect(it?.result, equals({}));
+      expect(it?.timeout, equals(Duration(seconds: 30)));
+      expect(it?.id, equals(id));
+    });
   });
 }
-
-const schema = 'job_queue';
-const init = [
-  'CREATE EXTENSION IF NOT EXISTS "pgcrypto";',
-  '''
-CREATE TABLE IF NOT EXISTS $schema.jobs (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    type TEXT NOT NULL,
-    payload JSONB NOT NULL,
-    status TEXT NOT NULL,
-    worker_id TEXT,
-    result JSONB,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    processing_started_at TIMESTAMPTZ,
-    timeout INTERVAL DEFAULT INTERVAL '30 minutes'
-);
-'''
-];
