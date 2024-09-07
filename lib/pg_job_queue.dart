93fc41ee0f968f116b1544d590b813b96f1dabe5
f3ath <karapetov@gmail.com>
2024-09-07 10:54:06 -0700

    WIP

--- lib/pg_job_queue.dart
+++ lib/pg_job_queue.dart
@@ -1,5 +1,6 @@
 library pg_job_queue;
 
+import 'package:collection/collection.dart';
 import 'package:postgres/postgres.dart';
 import 'package:uuid/uuid.dart';
 
@@ -9,53 +10,95 @@ class PgJobQueue {
     String schema = 'default',
     String table = 'jobs',
     String defaultQueue = 'default',
-    Duration defaultTimeout = const Duration(minutes: 30),
     String Function()? uniqueId,
   })  : _uniqueId = uniqueId ?? Uuid().v4,
         _defaultQueue = defaultQueue,
-        _defaultTimeout = defaultTimeout,
-        _insert = Sql.named('INSERT'
-            ' INTO $schema.$table (id, queue, payload, status)'
-            ' VALUES (@id, @queue, @payload, @status)'
-            ' RETURNING id'),
-        _fetch = Sql.named('SELECT * FROM $schema.$table WHERE id = @id'),
-        _acquire = Sql.named('UPDATE $schema.$table SET'
-            ' status = @new_status,'
-            ' worker_id = @worker,'
-            ' updated_at = now(),'
-            ' processing_started_at = now(),'
-            ' timeout = @timeout'
-            ' WHERE id = (SELECT id FROM $schema.jobs WHERE queue = @queue and status = @status ORDER BY created_at LIMIT 1)'
-            ' RETURNING *'),
-        _complete = Sql.named('UPDATE $schema.$table SET'
-            ' status = @status,'
-            ' result = @result,'
-            ' updated_at = now()'
-            ' WHERE id = @id'),
+        _insert = Sql.named('''
+INSERT
+INTO "$schema"."$table" (id, queue, payload, priority, status)
+VALUES (@id, @queue, @payload, @priority, '${JobStatus.scheduled.name}')
+RETURNING id'''
+            .trim()),
+        _fetch = Sql.named('''
+SELECT * FROM "$schema"."$table" WHERE id = @id'''
+            .trim()),
+        _acquire = Sql.named('''
+UPDATE "$schema"."$table" SET
+  status = '${JobStatus.acquired.name}',
+  worker = @worker,
+  updated_at = now()
+WHERE id = (
+  SELECT id FROM "$schema"."$table"
+  WHERE 
+    queue = @queue 
+    AND status = '${JobStatus.scheduled.name}'
+  ORDER BY priority DESC, created_at
+  LIMIT 1
+  FOR UPDATE SKIP LOCKED
+)
+RETURNING *'''
+            .trim()),
+        _complete = Sql.named('''
+UPDATE "$schema"."$table" SET
+  status = @status,
+  result = @result,
+  updated_at = now()
+WHERE 
+  id = @id
+  AND status = '${JobStatus.acquired.name}'
+'''
+            .trim()),
+        _countByQueueByStatus = Sql.named('''
+SELECT queue, status, count(*) as job_count 
+FROM "$schema"."$table"
+GROUP BY queue, status 
+ORDER BY queue, status'''
+            .trim()),
         _init = [
-          'CREATE TABLE IF NOT EXISTS $schema.$table ('
-              ' id TEXT PRIMARY KEY,'
-              ' queue TEXT NOT NULL,'
-              ' payload JSONB NOT NULL,'
-              ' status TEXT NOT NULL,'
-              ' worker_id TEXT,'
-              ' result JSONB,'
-              ' created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,'
-              ' updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,'
-              ' processing_started_at TIMESTAMPTZ,'
-              ' timeout INTERVAL'
-              ' );'
-        ];
+          '''
+CREATE TABLE IF NOT EXISTS "$schema"."$table" (
+  id text PRIMARY KEY,
+  queue text NOT NULL,
+  payload jsonb NOT NULL,
+  status text NOT NULL,
+  priority smallint NOT NULL,
+  worker text,
+  result jsonb,
+  created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamptz DEFAULT CURRENT_TIMESTAMP
+);''',
+          '''
+CREATE INDEX idx_${schema}_${table}_queue_status_priority_created_at
+ON "$schema"."$table" (queue, status, priority DESC, created_at);
+'''
+        ].map((s) => s.trim()).toList() {
+    if (!_schemaRegExp.hasMatch(schema)) {
+      throw ArgumentError.value(
+          schema, 'schema', 'Schema name must match $_schemaRegExp');
+    }
+    if (!_tableRegExp.hasMatch(table)) {
+      throw ArgumentError.value(
+          table, 'table', 'Table name must match $_tableRegExp');
+    }
+  }
+
+  static const maxPriority = 32767;
+  static const minPriority = -32768;
+
+  static final _schemaRegExp =
+      RegExp(r'^[a-z_][a-z0-9_]*$', caseSensitive: false);
+
+  static final _tableRegExp =
+      RegExp(r'^[a-z_][a-z0-9_]*$', caseSensitive: false);
 
   final Connection _connection;
-  final Duration _defaultTimeout;
-  final Sql _insert, _fetch, _acquire, _complete;
+  final Sql _insert, _fetch, _acquire, _complete, _countByQueueByStatus;
   final List<String> _init;
   final String Function() _uniqueId;
   final String _defaultQueue;
 
   /// Returns the list of SQL statements to initialize the job queue.
-  List<String> get initStatements => _init.toList(growable: false);
+  List<String> get initStatements => [..._init];
 
   /// Initializes the job queue.
   Future<void> init() async {
@@ -64,12 +107,17 @@ class PgJobQueue {
     }
   }
 
-  Future<String> add(Map<String, Object?> payload, {String? queue}) async {
+  Future<String> schedule(Map<String, Object?> payload,
+      {String? queue, int priority = 0}) async {
+    if (priority < minPriority || priority > maxPriority) {
+      throw ArgumentError.value(priority, 'priority',
+          'Priority must be between $minPriority and $maxPriority');
+    }
     final r = await _connection.execute(_insert, parameters: {
       'id': _uniqueId(),
       'queue': queue ?? _defaultQueue,
       'payload': payload,
-      'status': Status.scheduled.toString(),
+      'priority': priority,
     });
     return r.first.first as String;
   }
@@ -79,31 +127,39 @@ class PgJobQueue {
     return r.isEmpty ? null : r.first.toJob();
   }
 
-  Future<Job?> acquire(
-      {String? queue, Duration? timeout, String? worker}) async {
+  Future<Job?> acquire({String? queue, String? worker}) async {
     final r = await _connection.execute(_acquire, parameters: {
       'queue': queue ?? _defaultQueue,
       'worker': worker,
-      'status': Status.scheduled.toString(),
-      'new_status': Status.processing.toString(),
-      'timeout': TypedValue(Type.interval, timeout ?? _defaultTimeout),
     });
     return r.isEmpty ? null : r.first.toJob();
   }
 
   Future<void> complete(String id,
-      {Map<String, Object?> result = const {}}) async {
-    await _connection.execute(_complete, parameters: {
+      {Map<String, Object?> result = const {}, bool failed = false}) async {
+    final r = await _connection.execute(_complete, parameters: {
       'id': id,
-      'status': Status.completed.toString(),
+      'status': (failed ? JobStatus.failed : JobStatus.completed).name,
       'result': result,
     });
+    if (r.affectedRows != 1) {
+      throw StateError('Job not found or not in processing state: $id');
+    }
   }
-}
 
-extension on Interval? {
-  Duration? get duration =>
-      this == null ? null : Duration(microseconds: this!.microseconds);
+  Future<void> fail(String id, {Map<String, Object?> result = const {}}) =>
+      complete(id, result: result, failed: true);
+
+  Future<Map<String, Map<String, int>>> countByQueueByStatus() async {
+    final r = await _connection.execute(_countByQueueByStatus);
+    return groupBy(r.map((r) => r.toColumnMap()), (r) => r['queue'] as String)
+        .map((queue, rows) => MapEntry(
+            queue,
+            lastBy(rows, (r) => r['status'] as String)
+                .map((status, row) =>
+                    MapEntry(status, (row['job_count'] as int).toInt()))
+                .cast<String, int>()));
+  }
 }
 
 extension on ResultRow {
@@ -111,28 +167,30 @@ extension on ResultRow {
         {
           'id': String id,
           'queue': String queue,
+          'worker': String? worker,
           'payload': Map<String, Object?> payload,
+          'priority': int priority,
           'status': String status,
           'result': Map<String, Object?>? result,
-          'timeout': Interval? timeout,
         } =>
           Job(id, queue, payload,
-              Status.values.firstWhere((e) => e.toString() == status),
-              result: result, timeout: timeout.duration),
+              JobStatus.values.firstWhere((e) => e.name == status),
+              result: result, worker: worker, priority: priority),
         _ => throw StateError('Invalid row: $this'),
       };
 }
 
 class Job {
   Job(this.id, this.queue, this.payload, this.status,
-      {this.result, this.timeout});
+      {this.result, this.worker, this.priority = 0});
 
   final String id;
   final String queue;
+  final String? worker;
   final Map<String, Object?> payload;
   final Map<String, Object?>? result;
-  final Status status;
-  final Duration? timeout;
+  final JobStatus status;
+  final int priority;
 }
 
-enum Status { scheduled, processing, completed, failed }
+enum JobStatus { scheduled, acquired, completed, failed }
--- test/functional_test.dart
+++ test/functional_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:pg_job_queue/pg_job_queue.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
@@ -15,40 +17,289 @@ void main() async {
     await PgJobQueue(connection, schema: schema).init();
   });
 
-  test('Happy path', () async {
+  group('Happy path', () {
+    test('completed', () async {
+      final connection = await createConnection();
+      final queue = PgJobQueue(connection, schema: schema);
+      final payload = {'foo': 123, 'bar': 'baz.md'};
+      final id = await queue.schedule(payload);
+
+      await queue.fetch(id).then((job) {
+        expect(job, isNotNull);
+        expect(job?.payload, equals(payload));
+        expect(job?.queue, equals('default'));
+        expect(job?.status, equals(JobStatus.scheduled));
+        expect(job?.result, isNull);
+        expect(job?.id, equals(id));
+      });
+
+      await queue.acquire(worker: 'worker1').then((job) {
+        expect(job?.payload, equals(payload));
+        expect(job?.queue, equals('default'));
+        expect(job?.status, equals(JobStatus.acquired));
+        expect(job?.result, isNull);
+        expect(job?.id, equals(id));
+      });
+
+      await queue.complete(id);
+      await queue.fetch(id).then((job) {
+        expect(job?.payload, equals(payload));
+        expect(job?.queue, equals('default'));
+        expect(job?.status, equals(JobStatus.completed));
+        expect(job?.result, equals({}));
+        expect(job?.id, equals(id));
+      });
+    });
+
+    test('failed', () async {
+      final connection = await createConnection();
+      final queue = PgJobQueue(connection, schema: schema);
+      final payload = {'foo': 123, 'bar': 'baz.md'};
+      final id = await queue.schedule(payload);
+
+      await queue.fetch(id).then((job) {
+        expect(job, isNotNull);
+        expect(job?.payload, equals(payload));
+        expect(job?.queue, equals('default'));
+        expect(job?.status, equals(JobStatus.scheduled));
+        expect(job?.result, isNull);
+        expect(job?.id, equals(id));
+      });
+
+      await queue.acquire(worker: 'worker1').then((job) {
+        expect(job?.payload, equals(payload));
+        expect(job?.queue, equals('default'));
+        expect(job?.status, equals(JobStatus.acquired));
+        expect(job?.result, isNull);
+        expect(job?.id, equals(id));
+      });
+
+      await queue.fail(id);
+      await queue.fetch(id).then((job) {
+        expect(job?.payload, equals(payload));
+        expect(job?.queue, equals('default'));
+        expect(job?.status, equals(JobStatus.failed));
+        expect(job?.result, equals({}));
+        expect(job?.id, equals(id));
+      });
+    });
+  });
+
+  group('Argument validation', () {
+    test('Schema must be alphanumeric with underscores', () async {
+      final goodSchemas = ['test', 'test_1', 'test_1_2', 'fooBar', 'MOO'];
+      final badSchemas = [
+        'test-1',
+        'test 1',
+        'test!',
+        'test@',
+        'test#',
+        '',
+        '3'
+      ];
+      final connection = await createConnection();
+      for (final schema in goodSchemas) {
+        PgJobQueue(connection, schema: schema);
+      }
+      for (final schema in badSchemas) {
+        expect(
+          () => PgJobQueue(connection, schema: schema),
+          throwsArgumentError,
+        );
+      }
+    });
+
+    test('Table must be alphanumeric with underscores', () async {
+      final goodTables = ['test', 'test_1', 'test_1_2', 'fooBar', 'MOO'];
+      final badTables = [
+        'test-1',
+        'test 1',
+        'test!',
+        'test@',
+        'test#',
+        '',
+        '3'
+      ];
+      final connection = await createConnection();
+      for (final table in goodTables) {
+        PgJobQueue(connection, table: table);
+      }
+      for (final table in badTables) {
+        expect(
+          () => PgJobQueue(connection, table: table),
+          throwsArgumentError,
+        );
+      }
+    });
+
+    test('Priority must be within range', () async {
+      final connection = await createConnection();
+      final queue = PgJobQueue(connection, schema: schema);
+      final payload = {'foo': 123, 'bar': 'baz.md'};
+      expect(
+        () => queue.schedule(payload, priority: PgJobQueue.minPriority - 1),
+        throwsArgumentError,
+      );
+      expect(
+        () => queue.schedule(payload, priority: PgJobQueue.maxPriority + 1),
+        throwsArgumentError,
+      );
+    });
+  });
+
+  group('Status transition', () {
+    test('Can not complete a scheduled job', () async {
+      final connection = await createConnection();
+      final queue = PgJobQueue(connection, schema: schema);
+      final payload = {'foo': 123, 'bar': 'baz.md'};
+      final id = await queue.schedule(payload);
+      expectLater(queue.complete(id), throwsStateError);
+      expectLater(queue.fail(id), throwsStateError);
+      final job = await queue.fetch(id);
+      expect(job?.status, equals(JobStatus.scheduled));
+    });
+    test('Can not fail a scheduled job', () async {
+      final connection = await createConnection();
+      final queue = PgJobQueue(connection, schema: schema);
+      final payload = {'foo': 123, 'bar': 'baz.md'};
+      final id = await queue.schedule(payload);
+      expectLater(queue.complete(id), throwsStateError);
+      expectLater(queue.fail(id), throwsStateError);
+      final job = await queue.fetch(id);
+      expect(job?.status, equals(JobStatus.scheduled));
+    });
+    test('Can not complete a completed job', () async {
+      final connection = await createConnection();
+      final queue = PgJobQueue(connection, schema: schema);
+      final payload = {'foo': 123, 'bar': 'baz.md'};
+      final id = await queue.schedule(payload);
+      await queue.acquire(worker: 'worker1');
+      await queue.complete(id);
+      expectLater(queue.complete(id), throwsStateError);
+      expectLater(queue.fail(id), throwsStateError);
+    });
+    test('Can not complete a failed job', () async {
+      final connection = await createConnection();
+      final queue = PgJobQueue(connection, schema: schema);
+      final payload = {'foo': 123, 'bar': 'baz.md'};
+      final id = await queue.schedule(payload);
+      await queue.acquire(worker: 'worker1');
+      await queue.fail(id);
+      expectLater(queue.complete(id), throwsStateError);
+      expectLater(queue.fail(id), throwsStateError);
+    });
+  });
+
+  test('Higher priority gets acquired first', () async {
     final connection = await createConnection();
     final queue = PgJobQueue(connection, schema: schema);
     final payload = {'foo': 123, 'bar': 'baz.md'};
-    final id = await queue.add(payload);
+    for (var i = 0; i < 10; i++) {
+      await queue.schedule(payload);
+      await queue.schedule(payload, priority: 1);
+      await queue.schedule(payload, priority: 2);
+    }
+    for (var p = 2; p >= 0; p--) {
+      for (var i = 0; i < 10; i++) {
+        await queue.acquire().then((job) {
+          expect(job?.priority, equals(p));
+          return queue.complete(job!.id);
+        });
+      }
+    }
+  });
 
-    await queue.fetch(id).then((it) {
-      expect(it?.payload, equals(payload));
-      expect(it?.queue, equals('default'));
-      expect(it?.status, equals(Status.scheduled));
-      expect(it?.result, isNull);
-      expect(it?.timeout, isNull);
-      expect(it?.id, equals(id));
+  test('Can use min and max priority', () async {
+    final connection = await createConnection();
+    final queue = PgJobQueue(connection, schema: schema);
+    final payload = {'foo': 123, 'bar': 'baz.md'};
+    await queue.schedule(payload, priority: PgJobQueue.minPriority);
+    await queue.schedule(payload, priority: PgJobQueue.maxPriority);
+    await queue.acquire().then((job) {
+      expect(job?.priority, equals(PgJobQueue.maxPriority));
+      return queue.complete(job!.id);
     });
+    await queue.acquire().then((job) {
+      expect(job?.priority, equals(PgJobQueue.minPriority));
+      return queue.complete(job!.id);
+    });
+  });
 
-    await queue
-        .acquire(worker: 'worker1', timeout: Duration(seconds: 30))
-        .then((it) {
-      expect(it?.payload, equals(payload));
-      expect(it?.queue, equals('default'));
-      expect(it?.status, equals(Status.processing));
-      expect(it?.result, isNull);
-      expect(it?.timeout, equals(Duration(seconds: 30)));
-      expect(it?.id, equals(id));
+  test('Can acquire a job with a specific queue', () async {
+    final connection = await createConnection();
+    final queue = PgJobQueue(connection, schema: schema);
+    final payload = {'foo': 123, 'bar': 'baz.md'};
+    await queue.schedule(payload, queue: 'foo');
+    await queue.schedule(payload, queue: 'bar');
+    expect(await queue.acquire(), isNull);
+    expect(await queue.acquire(queue: 'baz'), isNull);
+    await queue.acquire(queue: 'foo').then((job) {
+      expect(job?.queue, equals('foo'));
     });
+    await queue.acquire(queue: 'bar').then((job) {
+      expect(job?.queue, equals('bar'));
+    });
+  });
 
-    await queue.complete(id);
-    await queue.fetch(id).then((it) {
-      expect(it?.payload, equals(payload));
-      expect(it?.queue, equals('default'));
-      expect(it?.status, equals(Status.completed));
-      expect(it?.result, equals({}));
-      expect(it?.timeout, equals(Duration(seconds: 30)));
-      expect(it?.id, equals(id));
-    });
+  test('Multiple workers', () async {
+    const batchSize = 1000;
+    final connection = await createConnection();
+    final queue = PgJobQueue(connection, schema: schema);
+    final payload = {'foo': 123, 'bar': 'baz.md'};
+    final ids = <String>[];
+    final futures = <Future>[];
+    for (var i = 0; i < batchSize; i++) {
+      ids.add(await queue.schedule(payload));
+      futures.add(Future.delayed(Duration(milliseconds: Random().nextInt(100)))
+          .then((_) => queue.acquire(worker: 'worker$i'))
+          .then((job) =>
+              Future.delayed(Duration(milliseconds: Random().nextInt(100)))
+                  .then((_) => queue.complete(job!.id))));
+    }
+    await Future.wait(futures);
+    expect(await queue.acquire(), isNull);
+    for (final id in ids) {
+      final job = await queue.fetch(id);
+      expect(job?.status, equals(JobStatus.completed));
+    }
+  });
+
+  test('Count by queue by status', () async {
+    final connection = await createConnection();
+    final queue = PgJobQueue(connection, schema: schema);
+    final payload = {'foo': 123, 'bar': 'baz.md'};
+    for (var i = 0; i < 5; i++) {
+      await queue.schedule(payload);
+      await queue.schedule(payload, queue: 'foo');
+      await queue.schedule(payload, queue: 'bar');
+      await queue.schedule(payload, queue: 'baz');
+    }
+    await queue.acquire(queue: 'foo').then((job) => queue.complete(job!.id));
+    await queue.acquire(queue: 'foo').then((job) => queue.complete(job!.id));
+    await queue.acquire(queue: 'foo').then((job) => queue.complete(job!.id));
+    await queue.acquire(queue: 'foo').then((job) => queue.fail(job!.id));
+    await queue.acquire(queue: 'foo').then((job) => queue.fail(job!.id));
+
+    await queue.acquire(queue: 'bar').then((job) => queue.complete(job!.id));
+    await queue.acquire(queue: 'bar').then((job) => queue.fail(job!.id));
+    await queue.acquire(queue: 'bar');
+    await queue.acquire(queue: 'baz');
+
+    final counts = await queue.countByQueueByStatus();
+
+    print('Status: "${JobStatus.acquired}"');
+    expect(
+        counts,
+        equals({
+          'bar': {
+            'acquired': 1,
+            'completed': 1,
+            'failed': 1,
+            'scheduled': 2
+          },
+          'baz': {'acquired': 1, 'scheduled': 4},
+          'default': {'scheduled': 5},
+          'foo': {'completed': 3, 'failed': 2}
+        }));
   });
 }
